f10c17d9eeb836fe284b0dcea1072c1157d7c3ca
Sendi Septian <sendi.s@smooets.com>
2017-01-18 10:01:20 +0700

    add file for completed project

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'sendi',
+            'email' => 'sendiseptian429@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+    }
+}
